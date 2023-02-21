3785a2e402e36c454159d8615cb501780d326696
TPGPL <TPGPL@users.noreply.github.com>
2023-02-21 15:39:50 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module pl.edu.pw.sortingvisualizer {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+
+    opens pl.edu.pw.sortingvisualizer to javafx.fxml;
+    exports pl.edu.pw.sortingvisualizer;
+}
