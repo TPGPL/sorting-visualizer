2e64511572bae964075d1400bb57a25fd29452dd
TPGPL <TPGPL@users.noreply.github.com>
2023-02-21 22:38:25 +0100

    Implement BubbleSort

---
+++ src/main/java/pl/edu/pw/sortingvisualizer/sorters/BubbleSort.java
@@ -0,0 +1,35 @@
+package pl.edu.pw.sortingvisualizer.sorters;
+
+import pl.edu.pw.sortingvisualizer.sortingevent.SortingEvent;
+import pl.edu.pw.sortingvisualizer.sortingevent.SortingEventType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static pl.edu.pw.sortingvisualizer.sorters.SortingUtils.swap;
+
+public class BubbleSort implements VisualizableSorter {
+    @Override
+    public List<SortingEvent> sort(double[] nums) {
+        if (nums == null) {
+            throw new IllegalArgumentException("The nums array must not be null.");
+        }
+
+        List<SortingEvent> events = new ArrayList<>();
+        boolean hasSwapped = false;
+
+        do {
+            for (int i = 0; i < nums.length - 1; i++) {
+                events.add(new SortingEvent(SortingEventType.Comparison, i, i + 1));
+
+                if (nums[i] > nums[i + 1]) {
+                    events.add(new SortingEvent(SortingEventType.Swap, i, i + 1));
+                    swap(nums, i, i + 1);
+                    hasSwapped = true;
+                }
+            }
+        } while (hasSwapped);
+
+        return events;
+    }
+}
