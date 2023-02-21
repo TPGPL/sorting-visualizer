ce9bc1b755573d1446afda57be505b28bbbb8195
TPGPL <TPGPL@users.noreply.github.com>
2023-02-21 15:54:06 +0100

    Rename template classes

--- src/main/java/pl/edu/pw/sortingvisualizer/AppController.java
+++ src/main/java/pl/edu/pw/sortingvisualizer/AppController.java
@@ -3,7 +3,7 @@ package pl.edu.pw.sortingvisualizer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class AppController {
     @FXML
     private Label welcomeText;
 
