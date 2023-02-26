7af85225e8737c2b5db8512df4ed501858665c78
TPGPL <TPGPL@users.noreply.github.com>
2023-02-26 23:33:12 +0100

    Add UI elements to Controller

--- src/main/java/pl/edu/pw/sortingvisualizer/AppController.java
+++ src/main/java/pl/edu/pw/sortingvisualizer/AppController.java
@@ -1,7 +1,10 @@
 package pl.edu.pw.sortingvisualizer;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.VBox;
 import pl.edu.pw.sortingvisualizer.sorters.SortingAlgorithm;
 
 public class AppController {
@@ -9,6 +12,16 @@ public class AppController {
     private final static SortingAlgorithm DEFAULT_SORTING_ALGORITHM = SortingAlgorithm.BubbleSort;
     @FXML
     private ChoiceBox<SortingAlgorithm> sortChoiceBox;
+    @FXML
+    private Slider delaySlider;
+    @FXML
+    private Slider sizeSlider;
+    @FXML
+    private Button generateButton;
+    @FXML
+    private Button sortButton;
+    @FXML
+    private VBox drawPanel;
 
     @FXML
     public void initialize() {
