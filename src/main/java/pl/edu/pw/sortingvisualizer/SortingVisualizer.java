2b309f85dd1667165b862933dad64af5a48b6376
TPGPL <TPGPL@users.noreply.github.com>
2023-02-26 22:31:57 +0100

    Update app windows parameters

--- src/main/java/pl/edu/pw/sortingvisualizer/SortingVisualizer.java
+++ src/main/java/pl/edu/pw/sortingvisualizer/SortingVisualizer.java
@@ -13,8 +13,9 @@ public class SortingVisualizer extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SortingVisualizer.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 640, 600);
         stage.setTitle("SortingVisualizer");
+        stage.setResizable(false);
         stage.getIcons().add(new Image(Objects.requireNonNull(SortingVisualizer.class.getResourceAsStream("icon.png"))));
         stage.setScene(scene);
         stage.show();
