378c2a3ede3e35b23d1077ab5546ff3b41a44eea
TPGPL <TPGPL@users.noreply.github.com>
2023-03-01 19:32:26 +0100

    Increase the window resolution

--- src/main/java/pl/edu/pw/sortingvisualizer/SortingVisualizer.java
+++ src/main/java/pl/edu/pw/sortingvisualizer/SortingVisualizer.java
@@ -13,7 +13,7 @@ public class SortingVisualizer extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(SortingVisualizer.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 640, 600);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("SortingVisualizer");
         stage.setResizable(false);
         stage.getIcons().add(new Image(Objects.requireNonNull(SortingVisualizer.class.getResourceAsStream("icon.png"))));
