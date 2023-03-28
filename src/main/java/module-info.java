54b3595c5a612b1eea02b93b955745bc474f6e57
TPGPL <TPGPL@users.noreply.github.com>
2023-03-28 21:55:23 +0200

    Remove unused dependencies

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,10 +2,6 @@ module pl.edu.pw.sortingvisualizer {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-
     opens pl.edu.pw.sortingvisualizer to javafx.fxml;
     exports pl.edu.pw.sortingvisualizer;
     exports pl.edu.pw.sortingvisualizer.utils;
