8b96b263766c8886f921b19b3307a1466102877d
TPGPL <TPGPL@users.noreply.github.com>
2023-03-28 22:05:15 +0200

    Export the sorters package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,9 @@ module pl.edu.pw.sortingvisualizer {
     requires javafx.fxml;
 
     opens pl.edu.pw.sortingvisualizer to javafx.fxml;
+    opens pl.edu.pw.sortingvisualizer.utils to javafx.fxml;
+    opens pl.edu.pw.sortingvisualizer.sorters to javafx.fxml;
     exports pl.edu.pw.sortingvisualizer;
     exports pl.edu.pw.sortingvisualizer.utils;
-    opens pl.edu.pw.sortingvisualizer.utils to javafx.fxml;
+    exports pl.edu.pw.sortingvisualizer.sorters;
 }
