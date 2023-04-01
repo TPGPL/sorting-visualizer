7e13f862f2e9c6481360f098bb7026a22a28fc83
TPGPL <mateuszkwiatkowski2002@gmail.com>
2023-04-02 01:40:10 +0200

    Add default array type constant to Properties

--- src/main/java/pl/edu/pw/sortingvisualizer/Properties.java
+++ src/main/java/pl/edu/pw/sortingvisualizer/Properties.java
@@ -1,10 +1,12 @@
 package pl.edu.pw.sortingvisualizer;
 
 import javafx.scene.paint.Color;
+import pl.edu.pw.sortingvisualizer.generators.GeneratorType;
 import pl.edu.pw.sortingvisualizer.sorters.SortingAlgorithm;
 
 public class Properties {
     public final static SortingAlgorithm DEFAULT_SORTING_ALGORITHM = SortingAlgorithm.BubbleSort;
+    public final static GeneratorType DEFAULT_ARRAY_TYPE = GeneratorType.Random;
     public final static Color DEFAULT_BAR_COLOR = Color.BLACK;
     public final static Color DEFAULT_COMPARISON_COLOR = Color.RED;
     public final static Color DEFAULT_SWAP_COLOR = Color.TURQUOISE;
