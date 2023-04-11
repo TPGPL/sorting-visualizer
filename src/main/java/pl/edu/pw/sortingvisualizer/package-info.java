a5c71ce6e6975ba6d1755a715145603d20d25cf8
TPGPL <TPGPL@users.noreply.github.com>
2023-04-12 01:23:37 +0200

    Add package info for main module

---
+++ src/main/java/pl/edu/pw/sortingvisualizer/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Pakiet udostępniający narzędzia wymagane do wizualizacji działania różnych algorytmów sortowania dla różnych wektorów początkowych.
+ */
+package pl.edu.pw.sortingvisualizer;
