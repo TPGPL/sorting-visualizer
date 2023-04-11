07ce5dc92d4d8a8adb314d07aadb36fb5024afdd
TPGPL <TPGPL@users.noreply.github.com>
2023-04-11 21:14:15 +0200

    Add Javadoc for utils package

--- src/main/java/pl/edu/pw/sortingvisualizer/utils/KillableThread.java
+++ src/main/java/pl/edu/pw/sortingvisualizer/utils/KillableThread.java
@@ -1,8 +1,17 @@
 package pl.edu.pw.sortingvisualizer.utils;
 
+/**
+ * Wątek z dodatkowym mechanizmem zatrzymywania jego działania.
+ */
 public class KillableThread extends Thread {
+    /**
+     * Określa, czy wątek powinien zostać zatrzymany.
+     */
     protected volatile boolean isKilled;
 
+    /**
+     * Wysyła komunikat o zatrzymaniu działania wątku.
+     */
     public void kill() {
         isKilled = true;
     }
--- src/main/java/pl/edu/pw/sortingvisualizer/utils/RectangleArrayUtils.java
+++ src/main/java/pl/edu/pw/sortingvisualizer/utils/RectangleArrayUtils.java
@@ -3,7 +3,17 @@ package pl.edu.pw.sortingvisualizer.utils;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+/**
+ * Funkcjonalności pomocnicze wykorzystywane do manipulowania wektorami obiektów Rectangle.
+ */
 public class RectangleArrayUtils {
+    /**
+     * Przekształca wektor liczb rzeczywistych na wektor obiektów Rectangle.
+     *
+     * @param nums      wektor liczb rzeczywistych
+     * @param areaWidth szerokość całego obszaru przeznaczonego na wizualizację wektora
+     * @return wektor obiektów Rectangle
+     */
     public static Rectangle[] convertDoubleToRectangleArray(double[] nums, double areaWidth) {
         if (nums == null) {
             throw new IllegalArgumentException("The array must not be null.");
